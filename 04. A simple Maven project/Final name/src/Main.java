7ddf45e171ccc2ad9a2bde75373ec0a2c54501ef
Alex <Alehandru753202@mail.ru>
2024-06-07 14:13:30 +0300

    Final

--- 04. A simple Maven project/Final name/src/Main.java
+++ 04. A simple Maven project/Final name/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
    public static void main(String[] args) {
       // Write your solution here
+      <!-- https://mvnrepository.com/artifact/org.hibernate/hibernate-core -->
+<dependency>
+    <groupId>org.hibernate</groupId>
+    <artifactId>hibernate-core</artifactId>
+    <version>5.3.3.Final</version>
+</dependency>
+
    }
 }
